b3e626427d518b8659ee4a7c8669c22301dbc193
Dmitrii <dmitriy.aifor@gmail.com>
2024-05-18 13:00:27 +0500

    Moi 1 commit

--- aksh_domain.py
+++ aksh_domain.py
@@ -1,4 +1,4 @@
-def calculate_risk_of_sequela(age: int, lg: int, ka: int, bleeding: int, charlsone: int, es: int, sequela: bool,
+def calculate_risk_of_sequela(age: int, lg: int, ka: int, bleeding: int, charlsone: int, es: float, sequela: bool,
                               dindo: int, access: bool, joint: bool):
     is_bleeding = bleeding >= 1000
     return 0.80806 + 0.04355 * age - 0.07624 * lg + 0.0245 * ka - 1.366 * int(is_bleeding) - 1.4601 * charlsone - 0.30636 * es - 1.37319 * int(
--- aksh_gui.py
+++ aksh_gui.py
@@ -1,1 +1,32 @@
-import tkinter
+from tkinter import *
+
+window = Tk()
+
+
+window.title('Прогнозирование осложнений АКШ')
+window.geometry ('450x550')
+window.resizable(width=False, height = False)
+
+frame = Frame(window)
+frame.place(relwidth=1,relheight=0.33)
+
+patient = Label(frame,text='Пациент', font='Arial 10',fg='green')
+secondname = Label(frame,text='*Фамилия:', font='Arial 8')
+name = Label(frame,text='*Имя',font='Arial 8')
+thirdname =Label(frame,text='*Отчество', font='Arial 8')
+birthday =Label(frame,text='*Дата рождения', font='Arial 8')
+ngosp =Label(frame,text='№ госпитализации', font='Arial 8')
+gospdate =Label(frame,text='Дата госпитализации', font='Arial 8')
+patient.place(x=3,y=4)
+secondname.place(x=3,y=25)
+name.place(x=3,y=65)
+thirdname.place(x=3,y=105)
+
+
+vvod1 = Entry(frame)
+vvod1.place(relheight=0.08,relwidth=0.25,x=10,y=45)
+vvod2 = Entry(frame)
+vvod2.place(relheight=0.08,relwidth=0.25,x=10,y=85)
+vvod3 = Entry(frame)
+vvod3.place(relheight=0.08,relwidth=0.25,x=10,y=125)
+window.mainloop()
